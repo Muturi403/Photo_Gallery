abea8fe391b99a25ae49cd0d7ed24b4e76ab5532
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-04 12:20:32 +0300

    this will add the request for the location

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Image, Category, Location
+
+def index(request):
+  all_photos = Image.objects.all()
+  all_category = Category.objects.all()
+  all_location = Location.objects.all()
+
+  if request.GET.get('location'):
+    images = Image.filter_by_location(request.GET.get('location'))
+    return render(request, 'Gallery templates/location.html', {'all_photos':images, 'all_category':all_category, 'location': request.GET.get('location')})
+
+  return render(request, 'Gallery templates/index.html', {'all_photos':all_photos, 'all_category':all_category, 'all_location':all_location})
+  
+def search_photo_category(request):
+  if 'search' in request.GET and request.GET['search']:
+    search_term = request.GET.get('search')
+    searchCategories = Image.search_image(search_term)
+    all_category = Category.objects.all()
+    all_location = Location.objects.all()
+
+    return render(request, 'Gallery templates/search.html', {'searchresults': searchCategories, 'searchterm':search_term, 'all_category':all_category, 'all_location': all_location})
+  else:
+    return redirect('home')
+
+def location(request):
+  all_location = Location.objects.all()
+  print(all_location)
+  return render(request, 'Gallery templates/location.html')
 
 # Create your views here.
