8eb93523aec02b7381fded9ed1a14cadc6cb7be4
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-06 21:02:26 +0300

    this will add migration chenges

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 class Location(models.Model):
   location = models.CharField(max_length=200)
 
@@ -37,8 +38,8 @@ class Image(models.Model):
   image = models.ImageField(upload_to='photos')
   image_name =  models.CharField(max_length=200)
   image_desc = models.TextField()
-  location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
-  category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
+  location_id = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
+  category_id= models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
   date_upload = models.DateTimeField(auto_now_add=True)
 
   def save_image(self):
